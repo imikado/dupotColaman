56e7fb15894b4370dc433b546911f20a63f4124f
imikado <imikado@gmail.com>
2019-01-22 23:35:39 +0100

    maj : ajout splashscreen

---
+++ app/src/main/java/mika/dupot/colaman/SplashscreenActivity.java
@@ -0,0 +1,30 @@
+package mika.dupot.colaman;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashscreenActivity extends AppCompatActivity {
+    private static int splashScreenTimeOut=3000;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent oIntent =new Intent(SplashscreenActivity.this,TitleActivity.class);
+                startActivity(oIntent);
+                finish();
+            }
+        },splashScreenTimeOut);
+    }
+}
